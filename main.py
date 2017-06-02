baf38fdc53609d95b130767e9d456b896e475580
Antreas Antoniou <antrikohs@gmail.com>
2017-06-02 16:18:27 +0100

    Merged main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collector import *
 from data_handler import *
 
+
 for i in range(1, 10000):
     url_to_char_id, url_to_char_name = create_dicts_from_character_id(i)
     save_multiple_images(url_to_char_id)
